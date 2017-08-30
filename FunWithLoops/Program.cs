42c876ba309b69eadda254e0d9220c0c19e8fb69
JackiePiedra <jackie.r.piedra@gmail.com>
2017-08-29 20:44:24 -0400

    In-class problems with loops

--- FunWithLoops/Program.cs
+++ FunWithLoops/Program.cs
@@ -10,6 +10,7 @@ namespace FunWithLoops
     {
         static void Main(string[] args)
         {
+            /*
             //foreach loops
             string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
             foreach (string month in months)    //the variable month only exists for this loop
@@ -53,7 +54,102 @@ namespace FunWithLoops
             {
                 Console.WriteLine("Your lucky number is "+number);
             }
+            string[] names = { "Jordan", "Max", "Peter" };
+            for(int i = 0; i< names.Length; i++)
+            {
+                names[i] = names[i].ToLower();
+                Console.WriteLine(names[i]);
+            }
+            for(int counter = 1; counter<=10; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+            //write out numbers from 50 to 100
+            for(int counter=50; counter<=100; counter++)
+            {
+                Console.WriteLine(counter);
+            }
 
+            string greetings = "My name is Jackie";
+            string[] words = greetings.Split();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+                if(words[i]=="Jackie")
+                {
+                    words[i] = "Max";
+                } 
+            }
+            Console.WriteLine(words[words.Length-1]);
+
+            //for loops gives you more control over where and when a loop stops
+            */
+
+
+            //-------------In-class problems------------------
+            //create and array called days with the elements monday, tuesday, etc.
+            //use a for loop to print the elements to the console
+            string[] daysOfTheWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            for(int i = 0; i< daysOfTheWeek.Length; i++)
+            {
+                Console.WriteLine(daysOfTheWeek[i]);
+            }
+
+
+            //create an int array size 25, put the numbers 1 to 25 in the array, & print those numbers to the console
+            int[] numbersTo25 = new int[25];
+            for(int i = 0; i < numbersTo25.Length; i++)
+            {
+                numbersTo25[i] = i+1;
+                Console.WriteLine(numbersTo25[i]);
+            }
+
+
+            //start with the string "Once upon a time" and create an array called storyWords using the split method
+            //reverse the order of the elements in the array and using a for loop, print each word
+            string storyWords = "Once upon a time";
+            string[] words = storyWords.Split(); //splitting it on the white space
+            for(int i = 0; i <words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+            Array.Reverse(words);
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            //while loops
+            Console.WriteLine("Do you want to play a game?");
+            string playAgain = Console.ReadLine();
+            while(playAgain == "yes")
+            {
+                Console.WriteLine("It's a rematch");
+                Console.WriteLine("Do you want to play again?");
+                playAgain = Console.ReadLine();
+            }
+
+            Console.WriteLine("What is your favorite Roy G. Biv color?");
+            string userColor = Console.ReadLine();
+            //bool isRoygbivColor = false;    // name boolean variables "is.."
+            while(userColor != "red")
+            {
+                Console.WriteLine("Sorry, that is not the correct color. Please try again..");
+            }
+            Console.WriteLine("That is correct.");  //this will run if the while loop condition not met
+
+
+            //do while loops
+            string playAgainToo;
+            do
+            {
+                Console.WriteLine("Do you want to build a snowman?");
+                Console.WriteLine("That was fun");
+                Console.WriteLine("Do you want to build another one?");
+                playAgainToo = Console.ReadLine();
+            }
+            while (playAgainToo == "yes");
         }
     }
 }
